389451aecb4e6112ac14b53cc846a363e7db522c
G00428161 <145337544+G00428161@users.noreply.github.com>
2026-09-24 11:02:40 +0100

    Add arrow function to display message in console

--- exercise2b.js
+++ exercise2b.js
@@ -1,6 +1,9 @@
+//Creates an arrow function with myVal as a parameter
 let message = (myVal)=>{
 
+    // Displays the Value of myVal in the console
     console.log(myVal);
 }
 
+// Calls the function and passes "Hello World!" as the value message (Hello World)
 message("Hello World!")
