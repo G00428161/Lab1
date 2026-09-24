ca9700710f65b93c31e52b45f82b03fb8c4cd15a
G00428161 <145337544+G00428161@users.noreply.github.com>
2026-09-24 11:06:29 +0100

    Refactor task management functions in exercise3.js

--- exercise3.js
+++ exercise3.js
@@ -1,17 +1,18 @@
+//Creates a list of tasks
 let tasks = ["work", "Eat", "Study"];
-
+//Adds a new task
 let addTask = (task) => {
     tasks.push(task);
     console.log(task + " has been added to my Tasks");
     return tasks.length;
 }
-
+//Displays all tasks
 let listAllTasks=() => {
     tasks.forEach((element) => {
         console.log(element);
     });
 }
-
+//Deletes a task
 let deleteTask = (task) => {
     let index = tasks.indexOf(task);
 
@@ -19,7 +20,7 @@ let deleteTask = (task) => {
     console.log(task + " has been removed from my Tasks");
     return tasks.length;
 }
-
+//Runs the function
 addTask("Sleep");
 listAllTasks();
 deleteTask("Eat");
